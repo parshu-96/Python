96bf31bc6879b762a9c1ccbbc43fa0933b5d4494
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-23 20:27:01 +0530

    Escape Sequence

---
+++ escape_sequence.py
@@ -0,0 +1,8 @@
+print("\"Hello\" \"world\"")
+print("Hello \" World \"from Parshu")
+print("I\'m Parshu")
+print("Line A\nLine B")
+print("\tHello\tWorld")
+print("This is backslash \\")
+print("This is double backslash \\\\")
+print("Hell\bo") #\b=Delete one character
