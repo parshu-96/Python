7aba8d199b19bb0d632c2b5de71264ef693e7846
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-23 22:16:30 +0530

    Basic Operators

---
+++ calculator.py
@@ -0,0 +1,11 @@
+print(2+3)
+print(2/4) #Float Division
+print(4/2) #Floating point division
+print(4//2) #Integer Division
+print(2//4) #Integer Division
+print(2**3) #2^3
+print(2**0.5) #Square root of 2
+print(round(2**0.5,4)) #Square root of 2 upto 4 digits round off
+print(2**3/2*6-4*(3-4/2))
+print(3%2)
+print((2+3)*2)
