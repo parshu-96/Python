758353475e4ec86254835df1f686a65de3e01139
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-23 22:24:21 +0530

    Variable and Naming conventions

---
+++ variable.py
@@ -0,0 +1,19 @@
+# number1=2
+# print(number1)
+# number1=4
+# print(number1)
+# name="Harshit"
+# print(name)
+# name=123
+# print(name)
+
+#Python is dynamic programming language
+#Rules for variable name
+# 1.You can't start with number
+# 2.Variable can start with alphabates, Underscore
+# 3. You can't use special symbol in variable name 
+
+#Naming conventions for variables
+user_name="Rohit"
+#Separate variables with multiple words by underscore (Snake case writing)
+
