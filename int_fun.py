5f30a20216a138f963b8b50ca81b96bbfe1aedab
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-24 20:57:44 +0530

    string to int/float conversion

---
+++ int_fun.py
@@ -0,0 +1,19 @@
+
+# number_first=input("Enter First Number :")
+# number_second=input("Enter second Number :")
+# total=number_first+number_second
+# print("Total is :"+total) #String was taken as input
+
+# number_first=int(input("Enter First Number :"))
+# number_second=int(input("Enter second Number :"))
+# total=number_first+number_second
+# print("Total is : "+str(total)) #String was taken as input
+
+# Float
+# '4' --> 4.0
+
+num1=str(4)
+num2=float("44")
+num3=int("33")
+print(num2+num3)
+# We can add int to float, final answer will be in float
