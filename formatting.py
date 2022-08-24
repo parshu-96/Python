dfab44b52608dc9c43775c3631e69f6b28942231
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-24 22:04:36 +0530

    String Formatting

---
+++ formatting.py
@@ -0,0 +1,8 @@
+name="Parshu"
+age=25
+# print("Hello "+name+" Your age is "+age)
+# Conventional way to print string with variables. Instead of this we can use string formatting
+print("Hello {} Your age is {}".format(name,age))  #python 3
+print(f"Hello {name} your age is {age}") #python 3.6
+print("Hello {} your age is {}".format(name,age+2))  #python 3
+print(f"Hello {name} your age is {age+2}") #python 3.6
