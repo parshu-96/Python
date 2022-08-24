a7a40b3cdeac25c42b892c39c882bda2ebf3adea
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-24 20:46:48 +0530

    User Input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+#user input using input function
+name=input("Type your name : ")
+print("Hello "+name)
+# Input always be taken as string
+age=input("What is your age? ")
+print("Your age is : "+age)
