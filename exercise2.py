410605f1e40154d63bfa305fe0b5b2dc0da39838
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-24 22:12:38 +0530

    Exercise2 Solution

---
+++ exercise2.py
@@ -0,0 +1,4 @@
+# Find average of 3 numbers
+num1,num2,num3=input("Enter 3 numbers: ").split()
+total=num1+num2+num3
+print(f"Average of {num1}, {num2}, {num3} is: {(int(num1)+int(num2)+int(num3))/3}")
