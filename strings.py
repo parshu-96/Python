dffbcde46874a50b89d25164dcba8fe9974be8c7
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-24 20:43:22 +0530

    String Basics

---
+++ strings.py
@@ -0,0 +1,11 @@
+from distutils.command.build_scripts import first_line_re
+
+
+first_name="Parshuram"
+last_name="Mahindrakar"
+full_name=first_name+" "+last_name
+print(full_name)
+# print(first_name+3) #Error 
+print(first_name+"3")
+print(first_name+str(3))  #str changes number to string
+print(first_name*3) #Print String 3 times
