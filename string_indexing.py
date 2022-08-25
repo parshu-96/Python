ff8155a0b061ca348cc4c156d65cdf4f0d0eef68
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-25 21:47:43 +0530

    String indexing

---
+++ string_indexing.py
@@ -0,0 +1,8 @@
+language="python"
+#print particular char 
+print(language[2])
+# print(language[9]) #Throws error
+
+# To  print last character of string
+print(language[-1])
+# From back side index starts from -1
