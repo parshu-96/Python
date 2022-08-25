389f917e77165327f0f117f926f5f3e76c1bbe1e
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-25 22:09:30 +0530

    Reversing String (Exercise 3)

---
+++ exercise3.py
@@ -0,0 +1,7 @@
+#Ask user for name, Print it in reverse order
+name=input("Please Enter Name: ")
+print(name[-1::-1])
+# Or You can use
+
+reverse=name[-1::-1]
+print(f"Reverse of your name is: {reverse}")
