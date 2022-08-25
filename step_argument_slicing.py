68865fa67702cd84d0572d697d60f212ffc15516
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-25 22:05:18 +0530

    String Step Argument Slicing

---
+++ step_argument_slicing.py
@@ -0,0 +1,6 @@
+lang="Python"
+# print("Parshu"[2:5])
+print("Parshuram"[0:5:2]) #Takes 2 steps
+print("Parshuram"[0::2]) #Takes 2 steps, from start to end
+print("Parshuram"[5::-1])
+print("Parshuram"[-1::-1]) #To reverse String
