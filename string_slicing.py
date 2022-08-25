b2e6728119cb750f6fcf047b758bd98da1bdbdc4
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-25 21:53:00 +0530

    String Slicing

---
+++ string_slicing.py
@@ -0,0 +1,9 @@
+lang="Python"
+# Syntax = [start argument : stop argument]
+print(lang[0:2]) #You'll get string as stop argument-1
+print(lang[2:4])
+print(lang[3:6])
+print(lang[-3:6])
+print(lang[:]) #Prints complete string
+print(lang[1:]) #prints all string as no stop argument
+print(lang[:3]) #auto starts from 0
