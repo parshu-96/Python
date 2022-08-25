0d22239df01f18bdc11aa317a00cb1a8d51e7617
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-25 22:32:57 +0530

    Strip method

---
+++ strip_method.py
@@ -0,0 +1,10 @@
+name="     Parshu     "
+dots="..........."
+
+print(name+dots) #to see space on right
+print(name.lstrip()+dots)
+print(name.rstrip()+dots)
+print(name.strip()+dots)
+
+name="Parsh     u"
+print(name.replace(" ",""))
