775b059ccca0d9e8c6fccf09c6b90a62fef815b4
parshu-96 <parshuvmahindrakar@gmail.com>
2022-08-30 21:06:10 +0530

    Check String is empty or not

---
+++ check_empty_or_not.py
@@ -0,0 +1,11 @@
+name="abc"
+if name: #true if string is non empty
+    print("Non empty")
+else:
+    print("String is empty")
+
+name=input("Enter your name : ")
+if name:
+    print(f"your name is {name}")
+else:
+    print("You didn't typed anything")
